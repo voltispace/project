650c92612c123ddf4cf4fe840ae65f70ca1275e1
unknown <cizettespace@gmail.com>
2026-09-24 01:00:48 +0100

    dice_roller

--- dice_roller.py
+++ dice_roller.py
@@ -1,38 +1,35 @@
 import random
-# def roll_dice():
-attempts = 5
 
 print("=" *22)
 print(f"\033[105mWELCOME TO DICE ROLLER\033[0m")
 print("=" *22)
     
     
-while True:
-    
-            try:  
-               choice = input("Wanna play? (y/n): ").lower()
-            except ValueError:
-                print("invalid selection, choose y or n")
-         
-            if choice == "n":
-                print("Thanks for coming bye") 
-                break  
-            try:
-                die_count = int(input("choose number of dice(1,2): "))
-           
-                
-                    
-                if choice == "y" and die_count == 1:
-                    die1 = random.randint(1, 6) 
-                    print(die1)
-                    
-                elif choice == "y" and die_count == 2:   
-                    die1 = random.randint(1, 6)
-                    die2 = random.randint(1, 6)
-                    print(f"{die1}, {die2}")
-                else:
-                    print("Decide if you wanna play or not") 
-            except ValueError:
-                     print("Select a valid input between 1 and 2")
 
-                
+def roll_dice():    
+    choice = input("Wanna play? (y/n): ").lower()
+          
+    if choice == "n":
+        print("Thanks for coming bye") 
+            
+    die_count = int(input("choose number of dice(1,2): "))
+                             
+    if choice == "y" and die_count == 1:
+        die1 = random.randint(1, 6) 
+        print(f"You rolled: {die1}")
+                    
+    elif choice == "y" and die_count == 2:   
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        total = die1 + die2
+        print(f"You rollled: {die1}, {die2}. Your total is {total}")
+    else:
+        print("Decide if you wanna play or not") 
+rounds = 1
+
+while rounds <= 5:
+        input(f"\n--- Round {rounds} --- Press Enter to roll...")
+        roll_dice()
+        rounds += 1  # Increment round count
+
+print("\nGame over! You've used all 5 rolls.")
